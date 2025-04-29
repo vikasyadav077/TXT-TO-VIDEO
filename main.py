24787da15f2c49eb272388487fa209f64eeac3f9
Popeye <127945393+Popeye68@users.noreply.github.com>
2025-04-29 19:30:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1715,21 +1715,8 @@ async def txt_handler(bot: Client, m: Message):
     await m.reply_text("🔰Done🔰")
     await m.reply_text("✨Thankyou For Choosing")
 
-from flask import Flask
-from threading import Thread
 
-# Start a simple web server
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot is online"
-
-def run_web():
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-
-# Start Flask in background, then the bot
+bot.run()
 if __name__ == "__main__":
-    Thread(target=run_web).start()
-    asyncio.run(main())  # your bot’s async entry point
+    asyncio.run(main())
 
